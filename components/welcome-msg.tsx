198d533a2fe839f0244090b2a25cf847267e7d2f
Suhaan Sharma <subaanfeetpics@gmail.com>
2024-12-30 21:57:13 +1100

    transactions form and some reusable components

--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -8,7 +8,7 @@ export const WelcomeMsg = () => {
     return (
         <div className="space-y-2 mb-4">
             <h2 className='text-2xl lg:text-4xl text-white font-medium'>
-                Welcuzz buzz{isLoaded ? "," : " "} {user?.firstName} 🥷🏿👋🏿
+                Welcuzz back{isLoaded ? "," : " "} {user?.firstName} 🥷🏿👋🏿
             </h2>
             <p className="text-sm lg:text-base text-[#89b6fd]">
                 This is your YN certified financial overview report.
